00166f564713f1114df4e0640218303183e28888
Andrey Mitko <77544144+andrey-mitko@users.noreply.github.com>
2022-03-09 23:33:59 -0500

    Added Scroll Animations

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+
 
 type Props = {
   className: string;
@@ -6,21 +8,23 @@ type Props = {
 
 const About = (props: Props) => {
   return (
-    <div id="about" className={props.className}>
-      <h2 className="mt-11 font-spaceMono font-bold text-3xl">About Me</h2>
-      <div className="mt-7 font-medium text-lg leading-loose ">
-        <p>
-          Self-taught iOS & Web Developer (Full-Stack) with a strong knowledge
-          of Swift, JavaScript, Git and design in Sketch and Figma.
-        </p>
-        <br />
-        <p>
-          I am interested in mobile app and web development as well as the
-          business side - marketing, management and administration.
-        </p>
+    <ScrollAnimation animateIn="animate__fadeIn" animateOut="animate__fadeOut">
+      <div id="about" className={props.className}>
+        <h2 className="mt-11 font-spaceMono font-bold text-3xl">About Me</h2>
+        <div className="mt-7 font-medium text-lg leading-loose ">
+          <p>
+            Self-taught iOS & Web Developer (Full-Stack) with a strong knowledge
+            of Swift, JavaScript, Git and design in Sketch and Figma.
+          </p>
+          <br />
+          <p>
+            I am interested in mobile app and web development as well as the
+            business side - marketing, management and administration.
+          </p>
+        </div>
+        <div className="h-0.5 mt-11 w-full bg-borderLines"></div>
       </div>
-      <div className="h-0.5 mt-11 w-full bg-borderLines"></div>
-    </div>
+    </ScrollAnimation>
   );
 };
 
