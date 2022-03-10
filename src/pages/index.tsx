c403221f38a9bc306b907004673e4acdf4f3026e
Andrey Mitko <77544144+andrey-mitko@users.noreply.github.com>
2022-03-10 12:01:52 -0500

    Added Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,23 @@ import SEO from "../components/seo";
 import Skills from "../components/skills";
 import Blog from "../components/Blog";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 // markup
 const IndexPage = () => {
   return (
-    <div>
+    <div className="text-black100 font-lato">
       <SEO title="Andrey Mitko" />
-      <main className="text-black100 font-lato mx-5 md:mx-16 lg:mx-32 xl:mx-44 2xl:mx-60">
+      <main className="mx-5 md:mx-16 lg:mx-32 xl:mx-44 2xl:mx-60">
         <Hero className="w-full h-screen flex flex-col justify-between" />
         <About />
         <Skills />
         <Blog />
         <Contact />
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
