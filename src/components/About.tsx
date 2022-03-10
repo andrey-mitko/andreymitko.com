d3ead183318e300acc9b46b9596eaedea1cc9c0d
Andrey Mitko <77544144+andrey-mitko@users.noreply.github.com>
2022-03-09 22:42:18 -0500

    Added "About Me" section

---
+++ src/components/About.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type Props = {
+  className: string;
+};
+
+const About = (props: Props) => {
+  return (
+    <div id="about" className={props.className}>
+      <h2 className="mt-11 font-spaceMono font-bold text-3xl">About Me</h2>
+      <div className="mt-7 font-medium text-lg leading-loose ">
+        <p>
+          Self-taught iOS & Web Developer (Full-Stack) with a strong knowledge
+          of Swift, JavaScript, Git and design in Sketch and Figma.
+        </p>
+        <br />
+        <p>
+          I am interested in mobile app and web development as well as the
+          business side - marketing, management and administration.
+        </p>
+      </div>
+      <div className="h-0.5 mt-11 w-full bg-borderLines"></div>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
-import Hero from "../components/hero";
+import Hero from "../components/Hero";
+import About from "../components/About";
 import SEO from "../components/seo";
-
-
-
 
 // markup
 const IndexPage = () => {
@@ -12,6 +10,7 @@ const IndexPage = () => {
       <SEO title="Andrey Mitko" />
       <main className="text-black100 font-lato mx-5 md:mx-16 lg:mx-32 xl:mx-44 2xl:mx-60">
         <Hero className="w-full h-screen flex flex-col justify-between" />
+        <About className="" />
       </main>
     </div>
   );
